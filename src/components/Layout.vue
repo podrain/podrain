8e7d1556d9123733dab171151216dfde51b75242
Joe Sweeney <joe.sweeney224@gmail.com>
2021-01-09 14:04:15 -0700

    got some images rendering

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -14,6 +14,6 @@
 
 <script>
 export default {
-    
+  
 }
 </script>
--- src/components/PodcastList.vue
+++ src/components/PodcastList.vue
@@ -1,15 +1,36 @@
 <template>
   <Layout>
-    <span class="text-white">This is just temporary</span>
+    <div class="flex flex-wrap">
+      <div 
+        class="w-1/3 flex-none"
+        v-for="pc in podcasts"
+        :key="pc._id"
+      >
+        <img :src="pc.meta.imageURL">
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
   import Layout from './Layout.vue'
+  import Helpers from '../Helpers'
 
   export default {
     components: {
       Layout
+    },
+
+    data() {
+      return {
+        podcasts: []
+      }
+    },
+
+    mounted() {
+      Helpers.dexieDB.podcasts.toArray().then(result => {
+        this.podcasts = result
+      })
     }
   }
 </script>
