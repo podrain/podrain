4e8874f72bd1e7a38160544900327c6f79ab00f0
Joe Sweeney <joe.sweeney224@gmail.com>
2021-01-09 19:56:34 -0700

    add more helpers

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,3 +1,23 @@
 export default {
-  dexieDB: null
+  dexieDB: null,
+
+  cleanHTMLString(string) {
+    return new DOMParser().parseFromString(string, 'text/html').body.textContent
+  },
+
+  floatToISO(float = 0) {
+    return new Date(float * 1000).toISOString().substr(11, 8)
+  },
+
+  iOS() {
+    return [
+      'iPad',
+      'iPhone',
+      'iPod',
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator'
+    ].includes(navigator.platform)
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
 }
