8b44b51c6b1ed29f9590e6cfd3a635b9f99cea13
Joe Sweeney <joe.sweeney224@gmail.com>
2021-01-17 18:42:08 -0700

    bugfix: playhead doesn't persist to DB when queue empty on app load

--- src/App.vue
+++ src/App.vue
@@ -60,14 +60,19 @@ export default {
     })
 
     this.$store.dispatch('getQueue').then(() => {
-      return Helpers.dexieDB.episodes
-        .filter(ep => ep.currently_playing == true)
-        .toArray().then(result => {
-          this.$store.dispatch('playEpisode', { id: result[0]._id })
-        })
+      if (this.$store.state.queue.length > 0) {
+        return Helpers.dexieDB.episodes
+          .filter(ep => ep.currently_playing == true)
+          .toArray().then(result => {
+            this.$store.dispatch('playEpisode', { id: result[0]._id })
+          }) 
+      } else {
+        return Promise.resolve()
+      }
     }).then(() => {
       setInterval(() => {
         Helpers.dexieDB.episodes.where({ _id: this.$store.state.playingEpisode._id }).modify({ playhead: this.$store.state.playingEpisode.playhead })
+        console.log(this.$store.state.playingEpisode.playhead)
       }, 5000)
     })
   }
