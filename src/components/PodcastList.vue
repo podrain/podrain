c736195c61e418cd8199324c6bcdf34b7c5a9691
Joe Sweeney <joe.sweeney224@gmail.com>
2021-01-17 18:08:59 -0700

    default message for when no podcasts

--- src/components/PodcastList.vue
+++ src/components/PodcastList.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-wrap">
+
+    <div v-if="podcasts.length > 0" class="flex flex-wrap">
       <div 
         class="w-1/3 flex-none"
         v-for="pc in podcasts"
@@ -7,6 +8,13 @@
         @click="$router.push(`/podcasts/${pc._id}`)"
       >
         <img :src="pc.meta.imageURL">
+      </div>
+    </div>
+
+    <div v-else class="text-white h-full flex-1 flex justify-center items-center">
+      <div class="flex flex-col">
+        There are no podcasts.
+        <button @click="$router.push('/podcasts/create')" class="mt-2 p-2 bg-green-500">Add some!</button>
       </div>
     </div>
 </template>
