cb4a6ba6b2e692c19f404bbdf68655d6ea2ba0f0
Joe Sweeney <joe.sweeney224@gmail.com>
2023-10-30 21:20:32 -0600

    upgrades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
